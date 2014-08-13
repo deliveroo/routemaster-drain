456bb21d3f026488ec3d7deb8baeea180cbebdd3
Julien Letessier <julien.letessier@gmail.com>
2014-08-13 18:25:31 +0200

    Adds filtering receiver

---
+++ routemaster/receiver/filtered.rb
@@ -0,0 +1,28 @@
+require 'routemaster/receiver/basic'
+require 'routemaster/dirty/filter'
+require 'routemaster/dirty/map'
+require 'delegate'
+require 'wisper'
+
+module Routemaster
+  module Receiver
+    class Filtered
+      extend Forwardable
+      include Wisper::Publisher
+
+      def initialize(app, options = {})
+        @topics   = options.fetch(:topics, [])
+        @redis    = options.fetch(:redis)
+        @basic    = Basic.new(app, options)
+        @map      = options.fetch(:dirty_map)
+        @filter   = Dirty::Filter.new(redis: @redis, topics: @topics)
+
+        @basic.subscribe(@filter, prefix: true)
+        @filter.on(:entity_changed) { |url| @map.mark(url) }
+        @map.on(:dirty_entity) { |url| publish(:sweep_needed) }
+      end
+
+      delegate [:call] => :@basic
+    end
+  end
+end
