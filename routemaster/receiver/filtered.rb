be26542b97380e7f80ac9762711ee1460b463ee6
Julien Letessier <julien.letessier@gmail.com>
2014-08-13 18:57:43 +0200

    Removes default map from Receiver::Filter

--- routemaster/receiver/filtered.rb
+++ routemaster/receiver/filtered.rb
@@ -13,12 +13,12 @@ module Routemaster
       def initialize(app, options = {})
         @redis    = options.fetch(:redis)
         @basic    = Basic.new(app, options)
-        @map      = options.fetch(:dirty_map) { Dirty::Map.new(@redis) }
+        @map      = options.fetch(:dirty_map)
         @filter   = Dirty::Filter.new(redis: @redis)
 
         @basic.subscribe(@filter, prefix: true)
         @filter.on(:entity_changed) { |url| @map.mark(url) }
-        @map.on(:dirty_entity) { |url| publish(:sweep_needed, @map) }
+        @map.on(:dirty_entity) { |url| publish(:sweep_needed) }
       end
 
       delegate [:call] => :@basic
