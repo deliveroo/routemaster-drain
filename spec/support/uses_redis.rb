fd277f975bfd52bcd61bf46f40ffee7e1303ca5e
Julien Letessier <julien.letessier@gmail.com>
2017-02-24 18:33:14 +0100

    Fixes warning noise

--- spec/support/uses_redis.rb
+++ spec/support/uses_redis.rb
@@ -7,8 +7,8 @@ module RspecSupportUsesRedis
     uses_dotenv
 
     let(:redis) { Routemaster::Config.drain_redis }
-    before { Routemaster::Config.cache_redis.flushdb }
-    before { Routemaster::Config.drain_redis.flushdb }
+    before { Routemaster::Config.cache_redis.redis.flushdb }
+    before { Routemaster::Config.drain_redis.redis.flushdb }
   end
 end
 
