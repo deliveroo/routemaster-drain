46bc5410a7bd003836e934ea08b4c3f2fe6beb4f
Daniel Cooper <daniel@14lines.com>
2017-04-05 13:50:58 +0100

    LT-437/cache-expire-drain

--- spec/routemaster/drain/expiring_cache_spec.rb
+++ spec/routemaster/drain/expiring_cache_spec.rb
@@ -3,11 +3,10 @@ require 'spec/support/rack_test'
 require 'spec/support/uses_redis'
 require 'spec/support/uses_dotenv'
 require 'spec/support/events'
-require 'routemaster/drain/caching'
-
+require 'routemaster/drain/expiring_cache'
 require 'json'
 
-describe Routemaster::Drain::Caching do
+describe Routemaster::Drain::ExpiringCache do
   uses_dotenv
   uses_redis
 
@@ -38,12 +37,7 @@ describe Routemaster::Drain::Caching do
   it 'increments the event index' do
     ei_double = double(increment: 1)
     allow(Routemaster::EventIndex).to receive(:new).and_return(ei_double)
-    expect(ei_double).to receive(:increment).exactly(4).times
-    perform
-  end
-
-  it 'schedules caching jobs' do
-    expect_any_instance_of(Routemaster::Jobs::Client).to receive(:enqueue).exactly(3).times
+    expect(ei_double).to receive(:increment).exactly(3).times
     perform
   end
 end
