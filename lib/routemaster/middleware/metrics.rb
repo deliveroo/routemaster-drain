9021973e17e6a7ca4b3ca627a552fd812c00dec2
tom <tommaso.pavese@deliveroo.co.uk>
2017-06-25 23:07:15 +0100

    Ensure that middlewares can accept a hash of options

--- lib/routemaster/middleware/metrics.rb
+++ lib/routemaster/middleware/metrics.rb
@@ -3,7 +3,7 @@ module Routemaster
     class Metrics
       INTERACTION_KEY = 'api_client'.freeze
 
-      def initialize(app, client: nil, source_peer: nil)
+      def initialize(app, client: nil, source_peer: nil, **_)
         @app    = app
         @client = client
         @source_peer = source_peer
