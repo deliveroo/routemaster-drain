333e661b207df1bbf1ef95435f50ce7905ed326f
Daniel Cooper <daniel@14lines.com>
2017-11-08 17:31:51 +0000

    Expire the key after setting the cache

--- lib/routemaster/lua/cache_service_response.lua
+++ lib/routemaster/lua/cache_service_response.lua
@@ -1,1 +1,2 @@
-redis.pcall('HMSET', KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5], ARGV[6]);
+redis.call('HMSET', KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4], ARGV[5], ARGV[6]);
+redis.call('EXPIRE', KEYS[1], ARGV[7]);
